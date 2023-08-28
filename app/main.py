f8006ec60d7ff3b2fdaf2abf7dcce8a6ec6fc47e
Alexis Martins <alexismartins9@hotmail.com>
2023-08-28 16:12:24 +0200

    feat: add user login entrypoint

--- app/main.py
+++ app/main.py
@@ -1,12 +1,33 @@
-from typing import Union
-from fastapi import FastAPI
+from .database import *
+from fastapi import FastAPI, Request, HTTPException
+from starlette.responses import RedirectResponse
 
-app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+"""
+    Handle 404 error and redirect to custom 404 page
+    @param request: Request
+    @param exc: HTTPException
+    @return RedirectResponse
+"""
+async def redirectionToNotFound(request: Request, exc: HTTPException):
+    return RedirectResponse('https://duckpass.ch/404.html')
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+
+exception_handlers = {404: redirectionToNotFound}
+app = FastAPI(exception_handlers=exception_handlers)    # Create FastAPI instance
+
+
+"""
+    Create user in database
+    @param username: str
+    @param email: str
+    @param key_hash: str
+    @param symmetric_key_encrypted: str
+    @param two_factor_auth: str
+    @param vault_password: str    
+"""
+@app.post("/create_user/")
+def createNewUser(username: str, email: str, key_hash: str, symmetric_key_encrypted: str, two_factor_auth: str, vault_password: str):
+    user_data = (username, email, key_hash, symmetric_key_encrypted, two_factor_auth, vault_password)
+    insertUpdateDeleteRequest(insertUser(), user_data)
+    return {"message": "User created successfully"}
