98c42899706115b48d418f2eb17823e5161ac1d0
Alexis Martins <alexismartins9@hotmail.com>
2023-08-29 11:42:17 +0200

    refactor: edit database schema

--- app/database.py
+++ app/database.py
@@ -73,25 +73,27 @@ def insertUpdateDeleteRequest(req, values):
     Database request
 """
 def database():
-     return """
-        DROP SCHEMA IF EXISTS duckpass CASCADE;
-        CREATE SCHEMA duckpass;
+    return """
+         DROP SCHEMA IF EXISTS duckpass CASCADE;
+         CREATE SCHEMA duckpass;
 
-        SET SEARCH_PATH TO duckpass;
+         SET SEARCH_PATH TO duckpass;
 
-        DROP TABLE IF EXISTS "User" CASCADE;
-        CREATE TABLE "User"
-        (
-            userId                 SERIAL,
-            username               VARCHAR(32),
-            email                  VARCHAR(256) UNIQUE,
-            keyHash     VARCHAR(2048) NOT NULL,
-            symmetricKeyEncrypted  VARCHAR(2048) NOT NULL,
-            twoFactorAuth VARCHAR(2048),
+         DROP TABLE IF EXISTS "User" CASCADE;
+         CREATE TABLE "User"
+         (
+             userId                 SERIAL,
+             username               VARCHAR(32),
+             email                  VARCHAR(256) UNIQUE,
+             keyHash     VARCHAR(2048) NOT NULL,
+             symmetricKeyEncrypted  VARCHAR(2048) NOT NULL,
+             twoFactorAuth VARCHAR(2048) DEFAULT '0',
+             verified BOOLEAN DEFAULT FALSE,
+             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
             vaultPassword bytea,
-            PRIMARY KEY (userId)
-        );
-    """
+             PRIMARY KEY (userId)
+         );
+     """
 
 
 """
