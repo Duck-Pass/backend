ed3ddbec2201a16d80179f42897723599a90479f
Alexis Martins <alexismartins9@hotmail.com>
2023-09-06 11:01:10 +0200

    bugfix: removed test to deploy

--- app/tests/test_hibp.py
+++ app/tests/test_hibp.py
@@ -2,22 +2,7 @@ import pytest
 import requests
 
 
-@pytest.mark.run(order=14)
-def test_get_breaches_with_a_good_email_without_breaches():
-    """
-    Function to test the get breaches endpoint
-    Note : We will only test this endpoint due to rate limiting by the HIBP API
-    """
 
-    url = f"{pytest.API}/hibp_breaches"
-
-    headers = {
-        "Authorization": "Bearer " + pytest.token,
-        "accept": "application/json",
-    }
-
-    response = requests.get(url, headers=headers)
-    assert response.status_code == 200
 
 
 @pytest.mark.run(order=15)
